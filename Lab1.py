7faeab1e8ad3ac940ce3d68f3342121b2f704507
sschuler01 <164068963+sschuler01@users.noreply.github.com>
2024-08-28 17:22:43 -0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,31 @@
+#the authors' names are: Serenity and Rylee
+print("Hello, world")
+print("Welcome to CPSC 207!")
+#note: /n functions like a return
+print("Hello\nWorld")
+print("How\nare\nyou?")
+print("Today", "is", "Wednesday") #note: connects separate sequences with space
+#note: called concatenation
+print("Today"+"is"+"Wednesday") #note: connects separate sequences without space
+
+print("123\n45\n6")
+
+print(1+2*3)
+print((1+2)*3)
+print(7/3) #note: division with remainder
+print(7//3) #note: division without remainder
+print(7%3)
+print(22/4)
+print(22//4)
+print(22%4) #note: modulus gives remainder (ex: 3 goes into 10 two times giving 9 but to get to 10 you must add 1, so the remainder is 1)
+print(3**2) #note: exponent
+print(2**3)
+#note: python follows order of operations
+
+print((4+3)//2)
+print(6%2)
+
+print(1+2*3**4)
+print(2+2)
+print("50 pounds is", 50*0.453592, "Kilograms")
+
